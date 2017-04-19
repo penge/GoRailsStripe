186b9e9544e2312eb3c5242a5c2c86008f01519a
penge <penge@Pavels-MacBook-Air.local>
2017-04-19 17:08:30 +0800

    Scaffold Episode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419075906) do
+ActiveRecord::Schema.define(version: 20170419090559) do
+
+  create_table "episodes", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
